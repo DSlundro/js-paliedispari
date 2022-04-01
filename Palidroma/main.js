6413c047b99357fec64ef41bb07911fd2144eaed
DSlundro <dragan.savic95@outlook.it>
2022-04-01 15:05:10 +0200

    palindroma

---
+++ Palidroma/main.js
@@ -0,0 +1,24 @@
+// Chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo all'utente d iinserire una parola
+let userWord = prompt('Inserisci la tua parola!');
+console.log(userWord)
+// divido la parola in singole lettere
+let wordSplitted = userWord.split('');
+console.log(wordSplitted);
+// inverto la parola splittata
+let wordReversed = wordSplitted.reverse();
+console.log(wordReversed);
+// ricostruisco la parola invrsa e splittata
+let wordRecostructed = wordReversed.join('');
+
+// inserisco la condizione per verificare se la parola iniziale e paroal finale siano uguali
+if (userWord === wordRecostructed){
+    console.log('La parola è palindroma');
+}
+else{
+    console.log('La parola non è palindroma');
+}
+
+
